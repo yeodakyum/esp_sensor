de851e76863e93705df03858a01f3ed736da4c2a
Dakyum Yeo <yeodakyum@gmail.com>
2022-05-09 21:00:04 +0900

    Update insert_temp.php

--- insert_temp.php
+++ insert_temp.php
@@ -1,16 +1,16 @@
 <?php
 
 if(isset($_GET["temperature"])) {
-   $temperature = $_GET["temperature"]; // get temperature value from HTTP GET
+   $temperature = $_GET["temperature"]; 
 
    $servername = "localhost";
-   $username = "ESP32";
-   $password = "esp32io.com";
+   $username = "admin";
+   $password = "password";
    $database_name = "db_esp32";
 
-   // Create MySQL connection fom PHP to MySQL server
+ 
    $connection = new mysqli($servername, $username, $password, $database_name);
-   // Check connection
+
    if ($connection->connect_error) {
       die("MySQL connection failed: " . $connection->connect_error);
    }
