25d5970c5c15813589ec600b9d97eec7b3e8b995
여다겸 <yeodakyum@yeodagyeom-ui-MacBookAir.local>
2022-05-09 20:56:48 +0900

    fix

--- esp32_web/esp32_web.ino
+++ esp32_web/esp32_web.ino
@@ -1,112 +1,57 @@
 
 
-#ifdef ESP32
-  #include <WiFi.h>
-  #include <HTTPClient.h>
-#else
-  #include <ESP8266WiFi.h>
-  #include <ESP8266HTTPClient.h>
-  #include <WiFiClient.h>
-#endif
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_BME280.h>
-\
-const char* ssid     = "ssid";
-const char* password = "password";
+const char WIFI_SSID[] = "test";
+const char WIFI_PASSWORD[] = "password";
 
-// REPLACE with your Domain name and URL path or IP address with path
-const char* serverName = "http://scapp.us/post-esp-data.php";
+String HOST_NAME = "http://192.168.0.19"; 
+String PATH_NAME   = "/insert_temp.php";
+String queryString = "?temperature=30.5";
 
-// Keep this API Key value to be compatible with the PHP code provided in the project page. 
-// If you change the apiKeyValue value, the PHP file /post-esp-data.php also needs to have the same key 
-String apiKeyValue = "tPmAT5Ab3j7F9";
+void web(){
+    HTTPClient http;
+    
+    http.begin(HOST_NAME + PATH_NAME + queryString); //HTTP
+  int httpCode = http.GET();
 
-String sensorName = "BME280";
-String sensorLocation = "Office";
+  // httpCode will be negative on error
+  if(httpCode > 0) {
+    // file found at server
+    if(httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
+    } else {
+      
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    }
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
 
-/*#include <SPI.h>
-#define BME_SCK 18
-#define BME_MISO 19
-#define BME_MOSI 23
-#define BME_CS 5*/
-
-#define SEALEVELPRESSURE_HPA (1013.25)
-
-Adafruit_BME280 bme;  // I2C
-//Adafruit_BME280 bme(BME_CS);  // hardware SPI
-//Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO, BME_SCK);  // software SPI
+  http.end();
+  return;
+}
 
 void setup() {
-  Serial.begin(115200);
-  
-  WiFi.begin(ssid, password);
+  Serial.begin(9600); 
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) { 
+  while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
+
   Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
-  // (you can also pass in a Wire library object like &Wire2)
-  bool status = bme.begin(0x76);
-  if (!status) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring or change I2C address!");
-    while (1);
-  }
+  web();
+
 }
 
 void loop() {
-  //Check WiFi connection status
-  if(WiFi.status()== WL_CONNECTED){
-    WiFiClient client;
-    HTTPClient http;
-    
-    // Your Domain name with URL path or IP address with path
-    http.begin(client, serverName);
-    
-    // Specify content-type header
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-    
-    // Prepare your HTTP POST request data
-    String httpRequestData = "api_key=" + apiKeyValue + "&sensor=" + sensorName
-                          + "&location=" + sensorLocation + "&value1=" + String(bme.readTemperature())
-                          + "&value2=" + String(bme.readHumidity()) + "&value3=" + String(bme.readPressure()/100.0F) + "";
-    Serial.print("httpRequestData: ");
-    Serial.println(httpRequestData);
-    
-    // You can comment the httpRequestData variable above
-    // then, use the httpRequestData variable below (for testing purposes without the BME280 sensor)
-    //String httpRequestData = "api_key=tPmAT5Ab3j7F9&sensor=BME280&location=Office&value1=24.75&value2=49.54&value3=1005.14";
 
-    // Send HTTP POST request
-    int httpResponseCode = http.POST(httpRequestData);
-     
-    // If you need an HTTP request with a content type: text/plain
-    //http.addHeader("Content-Type", "text/plain");
-    //int httpResponseCode = http.POST("Hello, World!");
-    
-    // If you need an HTTP request with a content type: application/json, use the following:
-    //http.addHeader("Content-Type", "application/json");
-    //int httpResponseCode = http.POST("{\"value1\":\"19\",\"value2\":\"67\",\"value3\":\"78\"}");
-        
-    if (httpResponseCode>0) {
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
-    }
-    else {
-      Serial.print("Error code: ");
-      Serial.println(httpResponseCode);
-    }
-    // Free resources
-    http.end();
-  }
-  else {
-    Serial.println("WiFi Disconnected");
-  }
-  //Send an HTTP POST request every 30 seconds
-  delay(30000);  
 }
